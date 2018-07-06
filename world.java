f1b1d1173c3146ed33b7444b9d220453b8dcb94e
priyankaperumal123 <40511498+priyankaperumal123@users.noreply.github.com>
2018-07-06 21:22:34 +0530

    Create world.java

---
+++ world.java
@@ -0,0 +1,15 @@
+      import java.io.*;
+      import java.util.*;
+      class world
+      {
+      public static void main(String args[] ) throws Exception 
+      {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      String line = br.readLine();
+      int N = Integer.parseInt(line);
+      for (int i = 1; i < N; i++) 
+      {
+              System.out.println("hello");
+        }
+       }
+  }
